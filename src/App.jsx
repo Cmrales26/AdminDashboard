ef3e126d8c70d3f3b850eade2df7d8062f25cfb7
Cmrales26 <camilomorales2615@gmail.com>
2024-04-09 12:42:22 -0500

    Calendar

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import Invoices from "./screens/invoices/index";
 import Contacts from "./screens/contacts/index";
 import { IsCollapsedProvider } from "./middleware/IsCollapsed";
 import Form from "./screens/profileForm/index";
+import Calendar from "./screens/calendar/index";
 // import Bar from "./screens/Bar";
 // import Line from "./screens/Line";
 // import Pie from "./screens/Pie";
 // import FAQ from "./screens/faq";
 // import Geography from "./screens/geography";
-// import Calendar from "./screens/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -33,12 +33,12 @@ function App() {
                 <Route path="/contacts" element={<Contacts />} />
                 <Route path="/invoices" element={<Invoices />} />
                 <Route path="/form" element={<Form />} />
+                <Route path="/calendar" element={<Calendar />} />
                 {/* <Route path="/bar" element={<Bar />} /> */}
                 {/* <Route path="/line" element={<Line />} /> */}
                 {/* <Route path="/pie" element={<Pie />} /> */}
                 {/* <Route path="/faq" element={<FAQ />} /> */}
                 {/* <Route path="/geography" element={<Geography />} /> */}
-                {/* <Route path="/calendar" element={<Calendar />} /> */}
               </Routes>
             </main>
           </div>
